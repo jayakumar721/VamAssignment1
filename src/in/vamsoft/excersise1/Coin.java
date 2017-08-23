29b2ae8e2dc6c3ca7b7feeebb98853bdc32e3099
jayakumar721 <kumarjaya721@gmail.com>
2017-08-23 15:11:31 +0530

    test

--- src/in/vamsoft/excersise1/Coin.java
+++ src/in/vamsoft/excersise1/Coin.java
@@ -2,7 +2,7 @@ package in.vamsoft.excersise1;
 public enum Coin {
   HEADS, TAILS;
   
-  /** Returns Coin.HEADS and Coin.TAILS with equal probability. */
+  
   
   public static Coin flip() {
     if (Math.random() < 0.5) {
